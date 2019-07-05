c0746645476db796ec47e289589188c4b0eff3cd
crisp-convention-4859 <slluong@uw.edu>
2019-07-05 17:20:46 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,7 +2,7 @@ def find_element_index(array, value_to_find)
   i = 0 
   while i < array.length do
     if array[i] == value_to_find
-      return array[i]
+      return array[value_to_find]
     elsif array[i] != value_to_find
       i += 1
     end
