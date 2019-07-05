83599f60f67f31ae30c991d8e068549a73d3b252
crisp-convention-4859 <slluong@uw.edu>
2019-07-05 08:30:49 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,18 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  counter = 0 
+  while counter <= array.length do
+    if array[counter] == value_to_find
+      break
+    else 
+      counter += 1
 end
 
 def find_max_value(array)
-  # Add your solution here
+  array.max_by do |element|
+  element.field
 end
 
 def find_min_value(array)
-  # Add your solution here
+  array.min_by do |element|
+  element.field
 end
