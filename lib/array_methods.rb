7cbd9f3b91d32cc1ef731960dc812956391b8962
crisp-convention-4859 <slluong@uw.edu>
2019-07-05 17:24:08 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,12 +1,5 @@
 def find_element_index(array, value_to_find)
-  i = 0 
-  while i < array.length do
-    if array[i] == value_to_find
-      return array[i].indexOf 
-    elsif array[i] != value_to_find
-      i += 1
-    end
-  end
+  array.index(value_to_find)
 end
 
 def find_max_value(array)
